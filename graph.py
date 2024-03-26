a385b3b1095c74fe8da5b9de48b5d96e0c190209
Kaushal Bhingaradia <75636044+kashrtx@users.noreply.github.com>
2024-03-26 15:44:39 -0400

    Added graph file

---
+++ graph.py
@@ -0,0 +1,16 @@
+"""
+This file is for the graphing function
+"""
+import tkinter as tk
+
+
+class Graph(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+if __name__ == "__main__":
+    graph = Graph()
+    graph.mainloop()
